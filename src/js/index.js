ae4cb1115ac71051b2d39147f8267a888c693b29
yankechao <yankechao@vipkid.com.cn>
2017-12-28 20:12:33 +0800

    setup antd

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,15 @@ import get from 'lodash/get'
 import isEmpty from 'lodash/isEmpty'
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import { Button } from 'antd'
 
 console.log('hello parcel')
 
 const Hello = props => (
-  <h1>{props.msg}</h1>
+  <div>
+    <h1>{props.msg}</h1>
+    <Button click={() => {alert('clicked')}}>this is a test</Button>
+  </div>
 )
 
 ReactDOM.render(<Hello msg={Date.now()}/>, document.getElementById('app'))
