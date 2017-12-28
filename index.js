0702c940eef0212e535c888de7a79994f9b9e033
yankechao <yankechao@vipkid.com.cn>
2017-12-28 19:42:43 +0800

    set up react

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+import get from 'lodash/get'
+import isEmpty from 'lodash/isEmpty'
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
 console.log('hello parcel')
+
+const Hello = props => (
+  <h1>hello</h1>
+)
+
+ReactDOM.render(<Hello/>, document.getElementById('app'))
