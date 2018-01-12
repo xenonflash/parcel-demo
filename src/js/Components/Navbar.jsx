6ae5a70da66d437258ba10573c30858a4b7276a9
yankechao <yankechao@vipkid.com.cn>
2018-01-12 21:24:01 +0800

    setup styled componnets

--- src/js/Components/Navbar.jsx
+++ src/js/Components/Navbar.jsx
@@ -1,16 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
-const style = {
-  width: "100%",
-  height: "60p",
-  background: "#ddd",
-  borderBottom: "1px solid #aaa",
-  boxShadow: "0 2px 10px #ddd"
-}
 const Navbar = props => (
-  <div style={style}>
-    Navbar
+  <div className={props.className}>
+    <h1>Home</h1>
+    <ul className="nav-links">
+      <Link to="/">Home</Link>
+      <Link to="/test">Test</Link>
+    </ul>
   </div>
 )
 
-export default Navbar
+export default styled(Navbar)`
+  width: 100%;
+  height: 60px;
+  background: #ddd;
+  borderBottom: 1px solid #aaa;
+  boxShadow: 0 2px 10px #ddd;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .nav-links{
+    a{
+      text-decoration: none;
+      color: #333;
+      margin-right: 30px;
+    }
+  }
+`
